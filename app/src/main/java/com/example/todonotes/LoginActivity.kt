c08a30a306d43e780f8659239e4bf6a8d6068c70
shannon <sanjeet.shannonitu25@gmail.com>
2020-04-04 18:53:24 +0530

    Login Successful

--- app/src/main/java/com/example/todonotes/LoginActivity.kt
+++ app/src/main/java/com/example/todonotes/LoginActivity.kt
@@ -1,12 +1,40 @@
 package com.example.todonotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var fullName:EditText
+    lateinit var userName:EditText
+    lateinit var login:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login = findViewById(R.id.buttonLogin)
+        fullName = findViewById(R.id.textInputFullName)
+        userName = findViewById(R.id.textInputUserName)
+
+        login.setOnClickListener {
+
+            var full_name = fullName.text.toString()
+            var user_name = userName.text.toString()
+
+            if(full_name?.length > 0 && user_name?.length > 0){
+                var intent:Intent = Intent(this,MainActivity::class.java)
+                intent.putExtra("fullName", full_name)
+                intent.putExtra("userName", user_name)
+                startActivity(intent)
+            } else {
+                var toast:Toast = Toast.makeText(applicationContext,"Enter Values",Toast.LENGTH_SHORT)
+                toast.show()
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/todonotes/MainActivity.kt
+++ app/src/main/java/com/example/todonotes/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.todonotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var intent: Intent = intent
+        var fullName:String = intent.getStringExtra("fullName")
+        var userName:String = intent.getStringExtra("userName")
+
+        supportActionBar?.title = fullName
     }
 }
